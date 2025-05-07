d95fb115b5b2c662d70cafea788dafabde447215
AnhChienVu <vuanhchien003@gmail.com>
2025-05-06 18:20:26 -0600

    Add Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from "@/components/ui/button"
+import Hero from './components/custom/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Button>Click me</Button>
+      {/* Hero Section */}
+      <Hero />
     </>
   )
 }
